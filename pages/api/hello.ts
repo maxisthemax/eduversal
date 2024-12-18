a43deb7c1bfef1c039284592e4b8735be14f3204
max <max@jagaapp.com>
2024-12-18 10:37:18 +0800

    minor fix

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,6 +3,7 @@ import { getIronSession } from "iron-session";
 
 //*lib
 import { SessionData, sessionOptions } from "@/lib/session";
+import prisma from "@/lib/prisma";
 
 export default async function handler(
   req: NextApiRequest,
@@ -18,5 +19,14 @@ export default async function handler(
 
   if (!email || !password) {
     return res.status(400).json({ message: "Email and password are required" });
+  }
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    res.status(200).json({
+      message: "Sign In successful",
+      user: { id: user.id, email: user.email },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Sign In failed", error });
   }
 }
