75f46f350b887ac97040b2edac7ba73646b108ad
max <max@jagaapp.com>
2024-12-18 10:27:45 +0800

    minor fix

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,12 +1,45 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getIronSession } from "iron-session";
+import bcrypt from "bcrypt";
 
-type ResponseData = {
-  message: string;
-};
+//*lib
+import { SessionData, sessionOptions } from "@/lib/session";
+import prisma from "@/lib/prisma";
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ message: "Hello from Next.js!" });
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Only POST requests are allowed" });
+  }
+  const { email, password }: { email: string; password: string } = req.body;
+  const session = await getIronSession<SessionData>(req, res, sessionOptions);
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  try {
+    // Find the user in the database
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      session.destroy();
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Create a session and store user data
+    session.id = user.id;
+    session.email = user.email;
+    session.isLoggedIn = true;
+    await session.save();
+
+    res.status(200).json({
+      message: "Sign In successful",
+      user: { id: user.id, email: user.email },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Sign In failed", error });
+  }
 }
