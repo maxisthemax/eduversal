fb1cb1b57196d2ae1e3270b49bb9e167974a2c52
max <max@jagaapp.com>
2024-12-18 11:00:29 +0800

    Revert "minor fix"
    This reverts commit 9c5b3277c3f106d6651811e427d64745b53cc2e3.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+// Ensure the prisma instance is reused across hot reloads in development
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
