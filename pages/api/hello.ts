25c25b6e22661fc9484a0aeb93dbafc2803f997a
max <max@jagaapp.com>
2024-12-18 11:00:31 +0800

    Revert "minor fix"
    This reverts commit a43deb7c1bfef1c039284592e4b8735be14f3204.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,7 +3,6 @@ import { getIronSession } from "iron-session";
 
 //*lib
 import { SessionData, sessionOptions } from "@/lib/session";
-import prisma from "@/lib/prisma";
 
 export default async function handler(
   req: NextApiRequest,
@@ -19,14 +18,5 @@ export default async function handler(
 
   if (!email || !password) {
     return res.status(400).json({ message: "Email and password are required" });
-  }
-  try {
-    const user = await prisma.user.findUnique({ where: { email } });
-    res.status(200).json({
-      message: "Sign In successful",
-      user: { id: user.id, email: user.email },
-    });
-  } catch (error) {
-    res.status(500).json({ message: "Sign In failed", error });
   }
 }
