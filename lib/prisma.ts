9c5b3277c3f106d6651811e427d64745b53cc2e3
max <max@jagaapp.com>
2024-12-18 10:39:40 +0800

    minor fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+let prisma: PrismaClient;
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
-
-// Ensure the prisma instance is reused across hot reloads in development
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
