8f4f54225c64252f4fbf9af3b4e7b7c568b812eb
max <max@jagaapp.com>
2024-12-18 11:00:24 +0800

    Revert "minor fix"
    This reverts commit cedbd2f987609fd77d66e65b4ca84b0d3356d846.

--- pages/api/auth/signIn.ts
+++ pages/api/auth/signIn.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getIronSession } from "iron-session";
+//import { getIronSession } from "iron-session";
 import bcrypt from "bcrypt";
 
 //*lib
-import { SessionData, sessionOptions } from "@/lib/session";
+//import { SessionData, sessionOptions } from "@/lib/session";
 import prisma from "@/lib/prisma";
 
 export default async function signInHandler(
@@ -15,7 +15,7 @@ export default async function signInHandler(
   }
 
   const { email, password }: { email: string; password: string } = req.body;
-  const session = await getIronSession<SessionData>(req, res, sessionOptions);
+  //const session = await getIronSession<SessionData>(req, res, sessionOptions);
 
   if (!email || !password) {
     return res.status(400).json({ message: "Email and password are required" });
@@ -26,15 +26,15 @@ export default async function signInHandler(
     const user = await prisma.user.findUnique({ where: { email } });
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      session.destroy();
+      //session.destroy();
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
     // Create a session and store user data
-    session.id = user.id;
-    session.email = user.email;
-    session.isLoggedIn = true;
-    await session.save();
+    //session.id = user.id;
+    //session.email = user.email;
+    //session.isLoggedIn = true;
+    //await session.save();
 
     res.status(200).json({
       message: "Sign In successful",
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
-//import { getIronSession } from "iron-session";
+import { getIronSession } from "iron-session";
 
 //*lib
-//import { SessionData, sessionOptions } from "@/lib/session";
-import prisma from "lib/prisma";
+import { SessionData, sessionOptions } from "@/lib/session";
+import prisma from "@/lib/prisma";
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,9 +13,9 @@ export default async function handler(
     return res.status(405).json({ message: "Only POST requests are allowed" });
   }
   const { email, password }: { email: string; password: string } = req.body;
-  // const session = await getIronSession<SessionData>(req, res, sessionOptions);
+  const session = await getIronSession<SessionData>(req, res, sessionOptions);
 
-  // console.log(session);
+  console.log(session);
 
   if (!email || !password) {
     return res.status(400).json({ message: "Email and password are required" });
