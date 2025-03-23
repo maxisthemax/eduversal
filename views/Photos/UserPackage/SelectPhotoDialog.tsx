38ff21b4b772f22c2fed412469fa8bb174faeec6
max <maxisthemax89@gmail.com>
2025-03-23 10:03:45 +0800

    minor fix

--- views/Photos/UserPackage/SelectPhotoDialog.tsx
+++ views/Photos/UserPackage/SelectPhotoDialog.tsx
@@ -6,10 +6,12 @@ import {
 } from "material-ui-popup-state/hooks";
 
 //*lodash
+import filter from "lodash/filter";
 import sumBy from "lodash/sumBy";
 import uniqBy from "lodash/uniqBy";
 import find from "lodash/find";
 import findIndex from "lodash/findIndex";
+import includes from "lodash/includes";
 
 //*material
 import Dialog from "@mui/material/Dialog";
@@ -94,6 +96,7 @@ function SelectPhotoDialogForm({
   children?: React.ReactNode;
   handleClose: () => void;
 }) {
+  const [mandatoryField, setMandatoryField] = useState([]);
   const { userPackage, setUserPackage } = useUserPackages();
   const item: UserPackageItemData =
     mode === "edit"
@@ -124,6 +127,9 @@ function SelectPhotoDialogForm({
   >(item.productVariationOptions);
 
   const handleProductVariationOption = () => {
+    const check = handleCheckMandatory();
+    if (check) return;
+
     const items = userPackage.items;
 
     items[userPackage.currentStage] = {
@@ -145,6 +151,34 @@ function SelectPhotoDialogForm({
       items,
     });
     handleClose();
+  };
+
+  const handleCheckMandatory = (
+    productVariationOptionsValue?: UserPackageItemDataProductVariationOption[]
+  ) => {
+    const mandatoryProductVariations = filter(
+      album.albumProductVariations,
+      "mandatory"
+    );
+    const missingProductVariations = filter(
+      mandatoryProductVariations,
+      ({ productVariation }) => {
+        return !find(productVariationOptionsValue ?? productVariationOptions, {
+          productVariationId: productVariation.id,
+        });
+      }
+    );
+
+    setMandatoryField(
+      missingProductVariations.map(
+        ({ productVariation_id }) => productVariation_id
+      )
+    );
+
+    if (missingProductVariations.length > 0) {
+      return true;
+    }
+    return false;
   };
 
   return (
@@ -169,64 +203,75 @@ function SelectPhotoDialogForm({
         </Stack>
         <Divider sx={{ mt: 3, mb: 3 }} />
         <Stack spacing={2}>
-          {album.albumProductVariations.map(({ productVariation }, index) => {
-            return (
-              <Box key={index}>
-                <ListItemText
-                  primary={
-                    productVariation.name +
-                    `${
-                      productVariation.is_downloadable
-                        ? " (Includes Soft Copy)"
+          {album.albumProductVariations.map(
+            ({ productVariation, productVariation_id }, index) => {
+              return (
+                <Box key={index}>
+                  <ListItemText
+                    primary={
+                      productVariation.name +
+                      `${
+                        productVariation.is_downloadable
+                          ? " (Includes Soft Copy)"
+                          : ""
+                      }`
+                    }
+                    secondary={productVariation.description}
+                    sx={
+                      includes(mandatoryField, productVariation_id)
+                        ? { color: "#E74D3C" }
+                        : {}
+                    }
+                  />
+                  <TextField
+                    error={includes(mandatoryField, productVariation_id)}
+                    helperText={
+                      includes(mandatoryField, productVariation_id)
+                        ? "Required"
                         : ""
-                    }`
-                  }
-                  secondary={productVariation.description}
-                />
-                <TextField
-                  value={
-                    find(productVariationOptions, {
-                      productVariationId: productVariation.id,
-                    })?.productVariationOptionId
-                  }
-                  select
-                  slotProps={{
-                    select: {
-                      displayEmpty: true,
-                      defaultValue: undefined,
-                    },
-                  }}
-                >
-                  <MenuItem
-                    value={undefined}
-                    onClick={() => {
-                      setProductVariationOptions(
-                        productVariationOptions.filter(
-                          (option) =>
-                            option.productVariationId !== productVariation.id
-                        )
-                      );
+                    }
+                    value={
+                      find(productVariationOptions, {
+                        productVariationId: productVariation.id,
+                      })?.productVariationOptionId
+                    }
+                    select
+                    slotProps={{
+                      select: {
+                        displayEmpty: true,
+                        defaultValue: undefined,
+                      },
                     }}
                   >
-                    <ListItemText primary={`None`} />
-                  </MenuItem>
-                  {productVariation.options.map(
-                    ({
-                      id,
-                      name,
-                      price_format,
-                      price,
-                      description,
-                      currency,
-                      preview_url,
-                    }) => {
-                      return (
-                        <MenuItem
-                          key={id}
-                          value={id}
-                          onClick={() => {
-                            setProductVariationOptions(
-                              uniqBy(
+                    <MenuItem
+                      value={undefined}
+                      onClick={() => {
+                        const value = productVariationOptions.filter(
+                          (option) =>
+                            option.productVariationId !== productVariation.id
+                        );
+                        setProductVariationOptions(value);
+                        handleCheckMandatory(value);
+                      }}
+                    >
+                      <ListItemText primary={`None`} />
+                    </MenuItem>
+                    {productVariation.options.map(
+                      ({
+                        id,
+                        name,
+                        price_format,
+                        price,
+                        description,
+                        currency,
+                        preview_url,
+                      }) => {
+                        return (
+                          <MenuItem
+                            key={id}
+                            value={id}
+                            onClick={() => {
+                              const value = uniqBy(
                                 [
                                   ...productVariationOptions,
                                   {
@@ -247,22 +292,24 @@ function SelectPhotoDialogForm({
                                   },
                                 ],
                                 "productVariationId"
-                              )
-                            );
-                          }}
-                        >
-                          <ListItemText
-                            primary={`${name} - ${price_format}`}
-                            secondary={description}
-                          />
-                        </MenuItem>
-                      );
-                    }
-                  )}
-                </TextField>
-              </Box>
-            );
-          })}
+                              );
+                              setProductVariationOptions(value);
+                              handleCheckMandatory(value);
+                            }}
+                          >
+                            <ListItemText
+                              primary={`${name} - ${price_format}`}
+                              secondary={description}
+                            />
+                          </MenuItem>
+                        );
+                      }
+                    )}
+                  </TextField>
+                </Box>
+              );
+            }
+          )}
         </Stack>
       </DialogContent>
       <DialogActions>
