295e5c6b8177175e75d9523bb1411db2c8a80b09
max <maxisthemax89@gmail.com>
2025-06-04 18:48:59 +0800

    fix: wrap Formik component in Paper for improved layout in ForgotPassword

--- views/ForgotPassword/index.tsx
+++ views/ForgotPassword/index.tsx
@@ -12,6 +12,7 @@ import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
 
 //*utils
 import axios from "@/utils/axios";
@@ -29,63 +30,65 @@ function ForgotPassword() {
 
   return (
     <Container maxWidth="sm" sx={{ alignContent: "center" }}>
-      <Formik
-        initialValues={{ email: "" }}
-        validationSchema={validationSchema}
-        onSubmit={async (values) => {
-          await axios.post("/auth/forgotPassword", values);
-          push("/signin");
-        }}
-      >
-        {({
-          values,
-          errors,
-          handleChange,
-          touched,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => {
-          const formProps = {
+      <Paper elevation={0}>
+        <Formik
+          initialValues={{ email: "" }}
+          validationSchema={validationSchema}
+          onSubmit={async (values) => {
+            await axios.post("/auth/forgotPassword", values);
+            push("/signin");
+          }}
+        >
+          {({
             values,
             errors,
+            handleChange,
             touched,
             handleBlur,
-            handleChange,
-          };
-          return (
-            <OverlayBox isLoading={isSubmitting}>
-              <Form onSubmit={handleSubmit}>
-                <Stack spacing={2} sx={{ p: 2, textAlign: "center" }}>
-                  <Typography variant="h4">
-                    <b>Forgot Password</b>
-                  </Typography>
-                  <Typography variant="body1" sx={{ pl: 5, pr: 5 }}>
-                    {
-                      "An email will be sent to you with instructions on how to reset your password"
-                    }
-                  </Typography>
-                  <TextFieldForm
-                    name="email"
-                    label="Email"
-                    formProps={formProps}
-                    props={{ required: true }}
-                  />
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    disabled={isSubmitting}
-                  >
-                    Submit
-                  </Button>
-                  <Button onClick={() => push("/signin")}>Back</Button>
-                </Stack>
-              </Form>
-            </OverlayBox>
-          );
-        }}
-      </Formik>
+            handleSubmit,
+            isSubmitting,
+          }) => {
+            const formProps = {
+              values,
+              errors,
+              touched,
+              handleBlur,
+              handleChange,
+            };
+            return (
+              <OverlayBox isLoading={isSubmitting}>
+                <Form onSubmit={handleSubmit}>
+                  <Stack spacing={2} sx={{ p: 2, textAlign: "center" }}>
+                    <Typography variant="h4">
+                      <b>Forgot Password</b>
+                    </Typography>
+                    <Typography variant="body1" sx={{ pl: 5, pr: 5 }}>
+                      {
+                        "An email will be sent to you with instructions on how to reset your password"
+                      }
+                    </Typography>
+                    <TextFieldForm
+                      name="email"
+                      label="Email"
+                      formProps={formProps}
+                      props={{ required: true }}
+                    />
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      disabled={isSubmitting}
+                    >
+                      Submit
+                    </Button>
+                    <Button onClick={() => push("/signin")}>Back</Button>
+                  </Stack>
+                </Form>
+              </OverlayBox>
+            );
+          }}
+        </Formik>
+      </Paper>
     </Container>
   );
 }
